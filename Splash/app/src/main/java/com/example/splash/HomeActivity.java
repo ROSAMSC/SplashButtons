6281ec4ea00a16a23e8e893257ec3cad7802b5a9
ROSAMSC <6227@itescam.edu.mx>
2021-04-26 00:50:26 -0500

    Splash con personalizacion de botones

---
+++ Splash/app/src/main/java/com/example/splash/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+}
